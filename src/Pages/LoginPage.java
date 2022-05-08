f1f70a2a8b731c7a4368ec9d7c2f12f700124f64
kushanbhai <kushanchoudhury6@gmail.com>
2022-05-08 18:06:58 +0530

    added/updated/deleted files...

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -3,5 +3,8 @@ package Pages;
 public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("Hello Login Page");
+	String un = "admin";
+	String pwd = "admin";
+	System.out.println("Logging in...");
 }
 }
